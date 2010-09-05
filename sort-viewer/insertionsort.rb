2c4461ad5213e8b49793034708afdec199e27247
Josh Cheek <josh.cheek@gmail.com>
2010-09-05 17:59:29 -0500

    add the insertionsort code

---
+++ sort-viewer/insertionsort.rb
@@ -0,0 +1,82 @@
+#!/usr/bin/env ruby
+
+def draw( values , colours=Hash.new )
+  $results ||= begin
+    at_exit do
+      puts "NUMBER OF IMAGES: #{$results.size}"
+      File.open "positions" , "w" do |file|
+        file.write Marshal.dump($results)
+      end
+    end
+    Array.new
+  end
+  $results << colours.merge(:values => values)
+end
+
+
+class InsertionSort
+  
+  def initialize(ary,&block)
+    block ||= lambda { |a,b| a.send :'<=>' , b }
+    @values   =  ary
+    @compare  =  block
+  end
+  
+  def sort!
+    draw true
+    @last = 0                # last index of the sorted left
+    values.size.times do
+      insert
+      @last += 1
+    end
+    draw true
+    values
+  end
+  
+  def sort
+    self.class.new( values.dup , &@compare ).sort!
+  end
+  
+private
+
+  def insert
+    to_move = @last
+    @blues = Array.new
+    until to_move <= 0 || @compare[ values[to_move] , values[to_move-1] ] >= 0
+      @blues << to_move
+      to_move -= 1
+      @reds = [to_move]
+      values[to_move] , values[to_move+1] = values[to_move+1] , values[to_move]
+      draw
+    end
+  end
+
+
+  def draw( all_white = false )
+    if all_white
+      super values.dup , :whites => values.dup
+    else
+      super values.dup , :reds => @reds.dup , :blues => @blues.dup , :whites => (0...values.size).to_a-@reds-@blues
+    end
+  end
+
+  attr_reader :values
+
+end
+
+
+
+class Array
+  def insertionsort(&block)
+    InsertionSort.new( self , &block ).sort
+  end  
+  def insertionsort!(&block)
+    InsertionSort.new( self , &block ).sort!
+  end
+end
+
+
+
+ary = (0...200).to_a.shuffle
+puts "Before sort: #{ary.inspect}"
+puts "After sort: #{ary.insertionsort!.inspect}"
