8b1215a42b38bcc5731914ceb1205eb86b421a36
Josh Cheek <josh.cheek@gmail.com>
2010-10-01 04:00:06 -0500

    clean up to_images, and fix bug from earlier cleaning

--- sort-viewer/to_images.rb
+++ sort-viewer/to_images.rb
@@ -1,72 +1,77 @@
 #!/usr/bin/env jruby
 
+# make the results dir
 require 'fileutils'
+dirname = './results'
+FileUtils.mkdir dirname unless File.exist? dirname
 
+# import the jruby classes
 include Java
 import javax.imageio.ImageIO
 import java.awt.image.BufferedImage
 import java.awt.image.WritableRaster
 
+# set relevant variables
+pixel_height = 2
+pixel_width  = 3
+images = Marshal.load File.read('positions')    # retrieve the processed data from the file
+height = values = pixels = nil                  # declare these in enclosing scope
 
-positions   = 'positions'
-dirname     = './results'
-FileUtils.mkdir dirname unless File.exist? dirname
-
+# converts colours, usually in the rgb hex format :'#FF0000' into the rgb format java array of [ red_int , green_int , blue_int ]
 def to_rgb(colour)
   case colour
-  when :red , 'red'
+  when :red
     :'#FF0000'
-  when :blue , 'blue'
+  when :blue
     :'#0000FF'
-  when :green , 'green'
+  when :green
     :'#00FF00'
-  when :white , 'white'
+  when :white
     :'#FFFFFF'
-  when :black , 'black'
+  when :black
     :'#000000'
   else
     colour
   end.to_s.delete('#').scan(/../).map { |component| component.to_i(16) }.to_java(:int)
 end
 
+# pass add_lines  a colour as a java array of rgb int values, 
+#                 an array of indexes to add
+#                 and a location for the line of eitehr :background or :foreground
+# it will add the lines to the picture that we are building
+add_lines = lambda do |colour,to_add,location|
+  to_add.each do |index|
+    start_y , end_y = (location == :background ? 0 : height) , (height - values[index]*pixel_height)    # the array can go from zero to the height of the value, or from the height of the value to the height of the canvas
+    start_y , end_y = end_y , start_y unless start_y < end_y                                            # ensure we are moving from low to high
+    for x in pixel_width*index ... pixel_width*index.next
+      (start_y...end_y).each { |y| pixels.set_pixel x , y , colour }
+    end
+  end
+end
 
-images = Marshal.load File.read('positions')
 
 images.each_with_index do |image,index|
-  filename = File.join dirname , "#{'%05d' % index}.gif"
-  next if File.exist? filename  
+  
+  # don't redo work that has already been done
+  filename = File.join dirname , '%05d.gif'%index
+  next if File.exist? filename
+  
+  # it takes a while, keep user updated
   puts "Drawing #{index}"
   
-  values       = image[:values]
-  pixel_height = 2
-  pixel_width  = 3
-  height       = values.size * pixel_height
-  width        = values.size * pixel_width
+  # find the image's picture values (order of sortedness), and determine its width and height
+  values  = image[:values]
+  height  = values.size * pixel_height
+  width   = values.size * pixel_width
 
+  # http://download.oracle.com/javase/6/docs/api/java/awt/image/BufferedImage.html
   canvas = BufferedImage.new width , height , BufferedImage::TYPE_INT_RGB
   pixels = canvas.get_raster
 
-  add_lines = lambda do |colour,to_add,location|
-    colours = to_rgb(colour)
-    to_add.each do |index|
-      start_y , end_y = [ location == :background ? 0 : height   ,   height - values[index]*pixel_height ].sort
-      for x in pixel_width*index ... start_x+pixel_width
-        for y in start_y ... end_y
-          pixels.set_pixel x , y , colours
-        end
-      end
-    end
-  end
-
-  image[:colors] ||= Array.new
-  image[:colors].each do |color,indexes|
-    add_lines[ color.to_s , indexes , :foreground ]
-  end
-
-  image[:background_colors] ||= Array.new
-  image[:background_colors].each do |color,indexes|
-    add_lines[ color.to_s , indexes , :background ]  
-  end
+  # extract the information about which values are which colours, and apply them to the image
+  ( image[:colors]            || [] ).each { |colour,indexes| add_lines.call to_rgb(colour) , indexes , :foreground }
+  ( image[:background_colors] || [] ).each { |colour,indexes| add_lines.call to_rgb(colour) , indexes , :background }
   
+  # write the image to file
   ImageIO.write canvas , "gif" , java.io.File.new(filename)
 end
