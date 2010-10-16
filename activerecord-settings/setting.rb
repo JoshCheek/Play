0775d6382c702c2f7df5af7a6a804dbef81ac9db
Josh Cheek <josh.cheek@gmail.com>
2010-10-16 04:09:12 -0500

    add context and first test

--- activerecord-settings/setting.rb
+++ activerecord-settings/setting.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'test/unit'
-require 'sqlite3'
 require 'rubygems'
+require 'sqlite3'
 require 'active_record'
 
 # set up the database
@@ -21,11 +21,23 @@ CreateSettings.migrate :up
 # run the unit tests
 class TestSettingModel < Test::Unit::TestCase
   
-  def self.verify( to_verify , &verification )
-    define_method "test_" << to_verify.gsub(/\s+/,'_') , &verification
+  def self.to_method_name(str)
+    str.gsub(/\s+/,'_').gsub(/\W+/,'')
   end
   
+  def self.context(context)
+    @context = to_method_name(context)
+  end
+  
+  def self.verify( to_verify , &verification )
+    prefix = "test_"
+    prefix << @context << '_' if @context && !context.empty?
+    define_method prefix << to_method_name(to_verify) , &verification
+  end
+  
+  context 'querying'
+  verify 'if setting DNE, should create it' do
+    assert_equal 0 , Setting.count
+    
+  end
 end
-
-
-
