1e75dbbe3356b8344240a8664c0fbf0ba0889504
Josh Cheek <josh.cheek@gmail.com>
2010-11-21 05:02:49 -0600

    finished day 1

--- land-of-lisp-itinerary/days/day_1.rb
+++ land-of-lisp-itinerary/days/day_1.rb
@@ -10,13 +10,18 @@ by 'Dec 18' do
   end
   
   read "Getting started with Lisp" => 15-20 do
-    discuss "Lisp dialects"
+    discuss  "Lisp dialects"
     complete "Install CLISP"
     complete "Launch the REPL, play with it for ≥10 minutes"
   end
   
   read "Creating your first Lisp program" => 21-30 do
-    
+    supplement "Allow user to play again"
+    supplement "Have the program keep a score"
+    show       "your program"
+    discuss    "your implementation"
   end
+  
+  discuss "your first impression"
   
 end
