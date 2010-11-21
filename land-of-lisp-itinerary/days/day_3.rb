1ad7c51df4bd3cd525ae3c552ed482daa382caa5
Josh Cheek <josh.cheek@gmail.com>
2010-11-21 07:25:01 -0600

    added a third day

---
+++ land-of-lisp-itinerary/days/day_3.rb
@@ -0,0 +1,10 @@
+by 'Dec 22' do
+  
+  read "Building a text game engine" => 67-84 do
+    complete    "the game engine"
+    discuss     "thoughts on the framework of the game engine we created"
+    supplement  "add another room, you choose where it fits in and how it interacts with this world"
+    show        "your supplemented version of the game"
+  end
+  
+end
