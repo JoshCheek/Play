ab91245063312426e6282999be6e2c2a026ca1b5
Josh Cheek <josh.cheek@gmail.com>
2010-11-22 04:33:19 -0600

    add day 5

---
+++ land-of-lisp-itinerary/days/day_5.rb
@@ -0,0 +1,15 @@
+by 'Dec 26' do
+  
+  read "Going beyond basic lists" => 107-128 do
+    supplement  "what do you think will happen if you print your circular list without setting *print-circle* ? try it and see"
+    complete    "install graphviz"
+    show        "your graph of the wizard's house"
+    discuss     "implications of the cons system for lists"
+    discuss     "difficulties with the graphviz library"
+    discuss     "how might different data structures be implemented with lists?"
+  end
+    
+  discuss  "how was your Christmas?"
+  show     "any fun stuff you got ^_^"
+    
+end
