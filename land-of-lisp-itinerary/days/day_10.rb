ab2e6d65a014ccaaa1700462585d161a14dd0c9c
Josh Cheek <josh.cheek@gmail.com>
2010-11-22 08:24:24 -0600

    add more comments and discussion

--- land-of-lisp-itinerary/days/day_10.rb
+++ land-of-lisp-itinerary/days/day_10.rb
@@ -1,9 +1,11 @@
 by 'Jan 5' do
 
   read "Dice of doom, a game written in the functional style" => 303-338 do
+    comment   "hey, minimax again!"
     complete  "Dice of doom"
     show      "your game off"
     discuss   "thoughts on the functional style"
+    discuss   "what makes tail-call optimization something that people talk so much about?"
   end
   
   discuss "any revelations, head-scratchers, or general comments"
