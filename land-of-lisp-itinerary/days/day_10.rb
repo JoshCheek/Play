3eb08ad639a23853612b75741bfa112a154affd6
Josh Cheek <josh.cheek@gmail.com>
2010-11-22 08:11:32 -0600

    add day 10

---
+++ land-of-lisp-itinerary/days/day_10.rb
@@ -0,0 +1,11 @@
+by 'Jan 5' do
+
+  read "Dice of doom, a game written in the functional style" => 303-338 do
+    complete  "Dice of doom"
+    show      "your game off"
+    discuss   "thoughts on the functional style"
+  end
+  
+  discuss "any revelations, head-scratchers, or general comments"
+
+end
