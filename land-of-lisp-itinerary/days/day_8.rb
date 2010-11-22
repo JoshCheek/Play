8275b6ce21361e754ee12e3cf32896dfedcee951
Josh Cheek <josh.cheek@gmail.com>
2010-11-22 05:48:08 -0600

    add day 8

---
+++ land-of-lisp-itinerary/days/day_8.rb
@@ -0,0 +1,15 @@
+by 'Jan 3' do
+
+  read 'Printing text with the format function' => 221-236 do
+    discuss     "formatting output in C vs Lisp"
+    supplement  "create a program that prints out pascal's triangle"
+    show        "your program"
+  end
+  
+  read 'Working with streams' => 237-252 do
+    discuss "compare streams on Lisp with streams from other languages"
+    discuss "compare Lisp's sockets with sockets from other languages"
+    discuss "why might you use a string steram?"
+  end
+  
+end
