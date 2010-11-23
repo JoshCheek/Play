571c80ad8a5e8a84811675697ad5ef765fdd7ebf
Josh Cheek <josh.cheek@gmail.com>
2010-11-23 09:20:52 -0600

    update text formatter to generate labels for nodes

--- land-of-lisp-itinerary/lib/itinerary/formatters/text.rb
+++ land-of-lisp-itinerary/lib/itinerary/formatters/text.rb
@@ -5,7 +5,8 @@ module Itinerary
       def self.format( nodes=[] , result=[] , depth=0 )
         result << format_header(nodes.shift) if nodes.first.kind_of?(Itinerary::Header)
         nodes.each do |node|
-          result << "#{'  '*depth}#{node}"
+          type = node.class.to_s.split('::').last.upcase
+          result << "#{'  '*depth}#{type}: #{node}"
           format node.children, result , depth.next
         end
         result.flatten
