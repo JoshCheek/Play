61fee4456004bd633ef1b2f06755a91b5c80cc7b
Josh Cheek <josh.cheek@gmail.com>
2011-02-05 22:32:30 -0600

    add whitespace t o stopwatch so it doesn't look so ugly

--- chem-timer/stop_watch.rb
+++ chem-timer/stop_watch.rb
@@ -1,24 +1,32 @@
 class StopWatch
+  
   def progress
     return 0 unless started?
     (Time.now - initial) % duration / duration
   end
+  
   def duration=(value)
     @duration = value
   end
+  
   def duration
     (@duration || 1).to_i
   end
+  
   def on_duration?
     started? && seconds_passed % duration == 0
   end
+  
   def started?
     @initial
   end
+  
   def to_s
     "%d:%02d:%02d" % hrs_mins_secs
   end
+  
   alias_method :time_passed , :to_s
+  
   def start
     if started?
       resume
@@ -26,9 +34,11 @@ class StopWatch
       @initial ||= Time.now
     end
   end
+  
   def initial
     @initial || Time.now
   end
+  
   def seconds_passed
     if paused?
       (Time.now - initial - paused_time).to_i
@@ -36,6 +46,7 @@ class StopWatch
       (Time.now - initial).to_i
     end
   end
+  
   def hrs_mins_secs
     total_seconds = seconds_passed
     hms = Array.new
@@ -45,17 +56,22 @@ class StopWatch
     end
     hms.reverse
   end
+  
   def paused?
     @pause_start
   end
+  
   def pause
     @pause_start ||= Time.now
   end
+  
   def resume
     @initial += paused_time
     @pause_start = nil
   end
+  
   def paused_time
     Time.now - @pause_start
   end
+  
 end
