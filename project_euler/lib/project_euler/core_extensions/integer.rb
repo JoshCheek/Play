a6ccc6b76c24a4fe467be798b8ab4c49b1807394
Josh Cheek <josh.cheek@gmail.com>
2011-06-25 00:00:07 -0500

    add factorization

--- project_euler/lib/project_euler/core_extensions/integer.rb
+++ project_euler/lib/project_euler/core_extensions/integer.rb
@@ -35,5 +35,22 @@ class Integer
   def prime?
     ProjectEuler::Primes.prime? self
   end
+  
+  def factors
+    each_combination = lambda do |ary|
+      return ary if ary.empty?
+      crnt = ary.shift
+      without_crnt = each_combination[ary]
+      with_crnt = without_crnt.dup
+      without_crnt.each { |factor| with_crnt << (factor*crnt) }
+      with_crnt << crnt
+      with_crnt.uniq!
+      with_crnt
+    end
+    results = each_combination[prime_factorization].sort
+    results.unshift(1)
+    results
+  end
+  
 end
 
--- project_euler/spec/lib/core_extensions/integer_spec.rb
+++ project_euler/spec/lib/core_extensions/integer_spec.rb
@@ -46,4 +46,30 @@ describe Integer do
     specify { 7.should be_prime }
     specify { 8.should_not be_prime }
   end
+  
+  describe '#factors' do
+    specify 'it should yield factors to a block' do 
+      1.factors { |factor| factor.should == 1 }
+    end
+    
+    specify 'it should return an enumerator if no block is given' do
+      1.factors.take(1).should == [1]
+    end
+    
+    specify { 1.factors.to_a.should == [1] }
+    specify { 2.factors.to_a.should == [1, 2]}
+    specify { 10.factors.to_a.should == [1, 2, 5, 10] }
+    specify { 36.factors.to_a.should == [1, 2, 3, 4, 6, 9, 12, 18, 36] }
+    specify do
+      37800.factors.to_a.should == [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14,
+        15, 18, 20, 21, 24, 25, 27, 28, 30, 35, 36, 40, 42, 45, 50, 54, 56, 60,
+        63, 70, 72, 75, 84, 90, 100, 105, 108, 120, 126, 135, 140, 150, 168, 175,
+        180, 189, 200, 210, 216, 225, 252, 270, 280, 300, 315, 350, 360, 378, 420,
+        450, 504, 525, 540, 600, 630, 675, 700, 756, 840, 900, 945, 1050, 1080,
+        1260, 1350, 1400, 1512, 1575, 1800, 1890, 2100, 2520, 2700, 3150, 3780,
+        4200, 4725, 5400, 6300, 7560, 9450, 12600, 18900, 37800
+      ]
+    end
+  end
 end
