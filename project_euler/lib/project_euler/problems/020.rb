31e0b48d20538cd0f93a211176b2a4079983ac78
Josh Cheek <josh.cheek@gmail.com>
2011-06-26 23:04:35 -0500

    solve problem 20

---
+++ project_euler/lib/project_euler/problems/020.rb
@@ -0,0 +1,12 @@
+require 'project_euler/core_extensions/array'
+require 'project_euler/core_extensions/integer'
+
+module ProjectEuler
+  class Problem20
+    
+    def solution
+      100.factorial.digits.sum
+    end
+    
+  end
+end
---
+++ project_euler/spec/problems/020_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'project_euler/problems/020'
+
+describe ProjectEuler::Problem20 do  
+  its(:solution) { should == 648 }
+end
