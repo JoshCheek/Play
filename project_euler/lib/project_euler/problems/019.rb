96f8859b1f2bbd24e574603e8f18e4f7a6944fb5
Josh Cheek <josh.cheek@gmail.com>
2011-06-26 22:59:14 -0500

    solve problem 019

---
+++ project_euler/lib/project_euler/problems/019.rb
@@ -0,0 +1,21 @@
+require 'date'
+require 'project_euler/core_extensions/integer'
+
+module ProjectEuler
+  class Problem19
+    
+    def solution
+      dates = Date.parse('1 Jan 1901') ... Date.parse('31 Dec 2000')
+      dates.count { |date| monday?(date) && first_of_month?(date) }
+    end
+    
+    def monday?(date)
+      date.wday.zero?
+    end
+    
+    def first_of_month?(date)
+      date.day.one?
+    end
+    
+  end
+end
---
+++ project_euler/spec/problems/019_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'project_euler/problems/019'
+
+describe ProjectEuler::Problem19 do  
+  its(:solution) { should == 171 }
+end
